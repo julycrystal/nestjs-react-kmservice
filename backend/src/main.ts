b2cdf2fee88fc6cc672455a05eaedc23da0252d3
julycrystal <erikxins@gmail.com>
2021-12-06 14:22:10 +0630

    static access config added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap () {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useLogger(app.get(Logger));
+  app.useStaticAssets(join(__dirname, '..', '..', 'uploads'));
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }))
