dd65fc47e681abf42d788aef9431c95bd11aee07
julycrystal <erikxins@gmail.com>
2021-12-08 21:40:23 +0630

    code reformatted

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,24 +1,26 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { JwtService } from '../jwt/jwt.service';
-import { Repository } from 'typeorm';
-import { ChangePasswordInput, changePasswordOutput } from './dto/change-password.dto';
-import { CreateUserInput, CreateUserOutput } from './dto/create-user.dto';
-import { DeleteUserOutput } from './dto/delete-user.dto';
-import { GetUserInput, GetUserOutput } from './dto/get-user.dto';
-import { GetUsersOutput } from './dto/get-users.dto';
-import { LoginInput, LoginOutput } from './dto/login.dto';
-import { MyProfileOutput } from './dto/my-profile.dto';
-import { UpdateUserInput, UpdateUserOutput } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
-import { Verification } from './entities/verification.entity';
-import { VerifyEmailInput, VerifyEmailOutput } from './dto/verify-email.dto';
-import { UploadProfilePictureOutput } from './dto/upload-profile-picture.dto';
-import { ConfigService } from '@nestjs/config';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { JwtService } from "../jwt/jwt.service";
+import { Repository } from "typeorm";
+import {
+  ChangePasswordInput,
+  changePasswordOutput,
+} from "./dto/change-password.dto";
+import { CreateUserInput, CreateUserOutput } from "./dto/create-user.dto";
+import { DeleteUserOutput } from "./dto/delete-user.dto";
+import { GetUserInput, GetUserOutput } from "./dto/get-user.dto";
+import { GetUsersOutput } from "./dto/get-users.dto";
+import { LoginInput, LoginOutput } from "./dto/login.dto";
+import { MyProfileOutput } from "./dto/my-profile.dto";
+import { UpdateUserInput, UpdateUserOutput } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
+import { Verification } from "./entities/verification.entity";
+import { VerifyEmailInput, VerifyEmailOutput } from "./dto/verify-email.dto";
+import { UploadProfilePictureOutput } from "./dto/upload-profile-picture.dto";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class UserService {
-
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
@@ -32,33 +34,47 @@ export class UserService {
     try {
       const user = await this.usersRepository.create(createUserDto);
       await this.usersRepository.save(user);
-      await this.verificationRepository.save(this.verificationRepository.create({ user }));
+      await this.verificationRepository.save(
+        this.verificationRepository.create({ user })
+      );
       //  send email here
       return {
         ok: true,
-      }
+      };
     } catch (error) {
-      if (error.code && error.code === '23505') {
-        throw new HttpException(`User with this email already exists.`, HttpStatus.BAD_REQUEST)
+      if (error.code && error.code === "23505") {
+        throw new HttpException(
+          `User with this email already exists.`,
+          HttpStatus.BAD_REQUEST
+        );
       }
       return {
         ok: false,
-        error: "Cannot create user."
-      }
+        error: "Cannot create user.",
+      };
     }
   }
 
   async login ({ email, password }: LoginInput): Promise<LoginOutput> {
     try {
-      const user = await this.usersRepository.findOne({ email }, { select: ['email', 'password', 'verified'] });
+      const user = await this.usersRepository.findOne(
+        { email },
+        { select: ["email", "password", "verified"] }
+      );
       if (!user) {
-        throw new HttpException('Invalid email / password.', HttpStatus.BAD_REQUEST)
+        throw new HttpException(
+          "Invalid email / password.",
+          HttpStatus.BAD_REQUEST
+        );
       }
       if (!user.verified) {
-        throw new HttpException('You need to verify your email first.', HttpStatus.BAD_REQUEST)
+        throw new HttpException(
+          "You need to verify your email first.",
+          HttpStatus.BAD_REQUEST
+        );
       }
-      if (!await user.checkPassword(password)) {
-        throw new HttpException('Incorrect Password', HttpStatus.BAD_REQUEST)
+      if (!(await user.checkPassword(password))) {
+        throw new HttpException("Incorrect Password", HttpStatus.BAD_REQUEST);
       }
       // generate token
       const loggedInUser = await this.usersRepository.findOne({ email });
@@ -67,13 +83,15 @@ export class UserService {
         ok: true,
         user: loggedInUser,
         token,
-      }
+      };
     } catch (error) {
-      if (error.status === 400) { throw error; }
+      if (error.status === 400) {
+        throw error;
+      }
       return {
         ok: false,
-        error: "Login Failed."
-      }
+        error: "Login Failed.",
+      };
     }
   }
 
@@ -82,13 +100,13 @@ export class UserService {
       const users = await this.usersRepository.find();
       return {
         ok: true,
-        users
-      }
+        users,
+      };
     } catch (error) {
       return {
         ok: false,
-        error: "Cannot get all users."
-      }
+        error: "Cannot get all users.",
+      };
     }
   }
 
@@ -96,36 +114,41 @@ export class UserService {
     try {
       const user = await this.usersRepository.findOne({ id });
       if (!user) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+        throw new HttpException("User not found", HttpStatus.NOT_FOUND);
       }
       return {
         ok: true,
-        user
-      }
+        user,
+      };
     } catch (error) {
       if (error && error.name && error.name === "HttpException") {
         throw error;
       }
       return {
         ok: false,
-        error: "Cannot get user."
-      }
+        error: "Cannot get user.",
+      };
     }
   }
 
-  async update ({ name, email, bio }: UpdateUserInput, authUser: User): Promise<UpdateUserOutput> {
+  async update (
+    { name, email, bio }: UpdateUserInput,
+    authUser: User
+  ): Promise<UpdateUserOutput> {
     try {
       const { id } = authUser;
       let user = await this.usersRepository.findOne({ id });
       if (!user) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+        throw new HttpException("User not found", HttpStatus.NOT_FOUND);
       }
       if (email) {
         user.email = email;
         user.verified = false;
 
         await this.verificationRepository.delete({ user: { id: user.id } });
-        await this.verificationRepository.save(this.verificationRepository.create({ user }));
+        await this.verificationRepository.save(
+          this.verificationRepository.create({ user })
+        );
       }
 
       if (name) {
@@ -141,49 +164,56 @@ export class UserService {
 
       return {
         ok: true,
-        user
-      }
-
+        user,
+      };
     } catch (error) {
       if (error.name && error.name === "HttpException") {
         throw error;
       }
       return {
         ok: false,
-        error: "Cannot update users."
-      }
+        error: "Cannot update users.",
+      };
     }
   }
 
-  async changePassword ({ oldPassword, newPassword }: ChangePasswordInput, authUser: User): Promise<changePasswordOutput> {
+  async changePassword (
+    { oldPassword, newPassword }: ChangePasswordInput,
+    authUser: User
+  ): Promise<changePasswordOutput> {
     try {
       const { id } = authUser;
-      let user = await this.usersRepository.findOne({ id }, { select: ['password'] });
+      let user = await this.usersRepository.findOne(
+        { id },
+        { select: ["password"] }
+      );
       if (!user) {
-        throw new HttpException('User not found.', HttpStatus.NOT_FOUND)
+        throw new HttpException("User not found.", HttpStatus.NOT_FOUND);
       }
       const isPasswordCorrect = await user.checkPassword(oldPassword);
       if (!isPasswordCorrect) {
-        throw new HttpException('Incorrect Password', HttpStatus.BAD_REQUEST)
-
+        throw new HttpException("Incorrect Password", HttpStatus.BAD_REQUEST);
       }
       if (await user.checkPassword(newPassword)) {
-        throw new HttpException('New password can\'t be same with old password', HttpStatus.BAD_REQUEST)
+        throw new HttpException(
+          "New password can't be same with old password",
+          HttpStatus.BAD_REQUEST
+        );
       }
       user = await this.usersRepository.findOne({ id });
       user.password = newPassword;
       await this.usersRepository.save(user);
       return {
         ok: true,
-      }
+      };
     } catch (error) {
       if (error.name === "HttpException") {
         throw error;
       }
       return {
         ok: false,
-        error: "Cannot change password."
-      }
+        error: "Cannot change password.",
+      };
     }
   }
 
@@ -191,20 +221,20 @@ export class UserService {
     try {
       const user = await this.usersRepository.findOne({ id });
       if (!user) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+        throw new HttpException("User not found", HttpStatus.NOT_FOUND);
       }
       return {
         ok: true,
-        user
-      }
+        user,
+      };
     } catch (error) {
       if (error.name && error.name === "HttpException") {
         throw error;
       }
       return {
         ok: false,
-        error: "Cannot get user."
-      }
+        error: "Cannot get user.",
+      };
     }
   }
 
@@ -213,50 +243,57 @@ export class UserService {
       const { id } = authUser;
       const user = await this.usersRepository.findOne({ id });
       if (!user) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+        throw new HttpException("User not found", HttpStatus.NOT_FOUND);
       }
       await this.usersRepository.delete({ id });
       return {
         ok: true,
-      }
+      };
     } catch (error) {
       if (error.name && error.name === "HttpException") {
         throw error;
       }
       return {
         ok: false,
-        error: "Cannot delete users."
-      }
+        error: "Cannot delete users.",
+      };
     }
   }
 
   async verifyEmail ({ code }: VerifyEmailInput): Promise<VerifyEmailOutput> {
     try {
-
-      const verification = await this.verificationRepository.findOne({ code }, { relations: ['user'] })
+      const verification = await this.verificationRepository.findOne(
+        { code },
+        { relations: ["user"] }
+      );
 
       if (!verification) {
-        throw new HttpException('Invalid verification code', HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+          "Invalid verification code",
+          HttpStatus.BAD_REQUEST
+        );
       }
 
-      const user = await this.usersRepository.findOne({ id: verification.user.id })
+      const user = await this.usersRepository.findOne({
+        id: verification.user.id,
+      });
       user.verified = true;
 
-      await this.verificationRepository.delete({ user: { id: user.id } })
+      await this.verificationRepository.delete({ user: { id: user.id } });
 
       await this.usersRepository.save(user);
 
       return {
         ok: true,
-      }
+      };
     } catch (error) {
       if (error.name && error.name === "HttpException") {
         throw error;
       }
       return {
         ok: false,
-        error: "Email Verification Failed"
-      }
+        error: "Email Verification Failed",
+      };
     }
   }
 
@@ -265,24 +302,24 @@ export class UserService {
     filename: string
   ): Promise<UploadProfilePictureOutput> {
     try {
-      const user = await this.usersRepository.findOne({ id: authUser.id })
+      const user = await this.usersRepository.findOne({ id: authUser.id });
       if (!user) {
-        throw new HttpException('User not found.', HttpStatus.BAD_REQUEST)
+        throw new HttpException("User not found.", HttpStatus.BAD_REQUEST);
       }
       if (user.id !== authUser.id) {
-        throw new HttpException('Permission denied..', HttpStatus.UNAUTHORIZED)
+        throw new HttpException("Permission denied..", HttpStatus.UNAUTHORIZED);
       }
-      user.picture = this.configService.get('END_POINT') + `/profile/${filename}`;
+      user.picture =
+        this.configService.get("END_POINT") + `/profile/${filename}`;
       await this.usersRepository.save(user);
       return {
         ok: true,
-      }
+      };
     } catch (error) {
       return {
         ok: false,
-        error: "Cannot upload profile picture."
-      }
+        error: "Cannot upload profile picture.",
+      };
     }
   }
-
 }
