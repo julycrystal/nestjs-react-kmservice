9f2a8545a3e79dd996ae6d5d5fff9c602b4dc0f6
julycrystal <erikxins@gmail.com>
2021-12-09 20:21:09 +0630

    create product dto added

---
+++ backend/src/product/dto/create-product.dto.ts
@@ -0,0 +1,25 @@
+import { InputType, Field, PickType, ObjectType } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
+import { CoreOutput } from 'src/common/dtos/core.output';
+import { Product } from '../entities/product.entity';
+
+@InputType()
+export class CreateProductInput extends PickType(
+  Product,
+  [
+    'title',
+    'description',
+    'price',
+    'discount',
+    'showRemaining',
+  ]
+) {
+
+  @Field(() => String,)
+  @IsString()
+  categoryName: string;
+
+}
+
+@ObjectType()
+export class CreateProductOutput extends CoreOutput { }
