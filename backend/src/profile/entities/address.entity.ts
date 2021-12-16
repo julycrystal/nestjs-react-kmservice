d37dcc7b0969097683ec9d2c14c7cacb3e7c7ff3
julycrystal <erikxins@gmail.com>
2021-12-16 14:01:14 +0630

    AddressInputType name added

--- backend/src/profile/entities/address.entity.ts
+++ backend/src/profile/entities/address.entity.ts
@@ -2,10 +2,11 @@ import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { Type } from "class-transformer";
 import { IsOptional, IsString } from "class-validator";
 import { CoreEntity } from "src/common/entities/core.entity";
+import { Order } from "src/order/entities/order.entity";
 import { User } from "src/user/entities/user.entity";
-import { Column, Entity, ManyToOne } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
 
-@InputType({ isAbstract: true })
+@InputType("AddressInput", { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Address extends CoreEntity {
