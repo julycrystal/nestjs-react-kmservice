657f7e5e607df53fabef88169187b0ab0cd97160
julycrystal <erikxins@gmail.com>
2021-12-20 12:44:20 +0630

    auth slice added

---
+++ frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "./types/user.type";
+
+export interface IAuthState {
+    user?: IUser;
+}
+
+const initialState: IAuthState = {
+    user: undefined,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action: PayloadAction<IAuthState>) => {
+            state.user = action.payload.user;
+        },
+        register: (state,) => { },
+        logout: (state,) => { },
+        myProfile: (state) => { },
+        deleteAccount: (state) => { },
+    }
+})
+
+export const {
+    login,
+    register,
+    logout,
+    myProfile,
+    deleteAccount,
+} = authSlice.actions;
+
+export default authSlice.reducer;
