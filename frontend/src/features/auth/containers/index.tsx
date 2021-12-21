5b575fbec95486280d9cd9199a689e22cd69a591
julycrystal <erikxins@gmail.com>
2021-12-21 14:44:37 +0630

    auth route added

--- frontend/src/features/auth/containers/index.tsx
+++ frontend/src/features/auth/containers/index.tsx
@@ -1,2 +1,15 @@
+import { Route, Routes } from "react-router";
+import { Login } from "./Login";
+import { Register } from "./Register";
+
 export { Login } from "./Login";
 export { Register } from "./Register";
+
+export default function Auth () {
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    )
+}
