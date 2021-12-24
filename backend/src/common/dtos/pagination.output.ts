a7a954f9f029fd1da67c726e944d1a5e9c386198
julycrystal <erikxins@gmail.com>
2021-12-24 10:59:07 +0630

    grpahql set up for pagination dto

--- backend/src/common/dtos/pagination.output.ts
+++ backend/src/common/dtos/pagination.output.ts
@@ -1,20 +1,35 @@
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
 import { Type } from "class-transformer";
 import { IsNumber, IsOptional } from "class-validator";
 
+@ObjectType()
 export class PaginationOutput {
+    @Field(() => Number,)
     totalPages: number;
+
+    @Field(() => Number,)
     totalItems: number;
+
+    @Field(() => Number,)
     limit: number;
+
+    @Field(() => Number,)
     currentPage: number;
+
+    @Field(() => Number,)
     currentPageItems: number;
 }
 
+@InputType("PaginationInput", { isAbstract: true })
+@ObjectType()
 export class PaginationInput {
+    @Field(() => Number, { defaultValue: 1 })
     @IsNumber()
     @Type(() => Number)
     @IsOptional()
     pageNumber?: number;
 
+    @Field(() => Number, { defaultValue: 10 })
     @IsNumber()
     @Type(() => Number)
     @IsOptional()
