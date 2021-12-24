27facb6bf09188449dc4d217174a8d3287359d33
julycrystal <erikxins@gmail.com>
2021-12-24 11:06:08 +0630

    getprduct entries dto added

--- backend/src/product/dto/productEntry/get-product-entries.dto.ts
+++ backend/src/product/dto/productEntry/get-product-entries.dto.ts
@@ -1,15 +1,25 @@
 import { Field, InputType, Int, ObjectType } from "@nestjs/graphql";
+import { IsNumber } from "class-validator";
 import { CoreOutput } from "src/common/dtos/core.output";
+import { PaginationInput, PaginationOutput } from "src/common/dtos/pagination.output";
 import { ProductEntry } from "src/product/entities/product.entity";
 
-@InputType()
-export class GetProductEntriesInput {
+@InputType({ isAbstract: true })
+export class GetProductEntriesInput extends PaginationInput {
     @Field(() => Int)
-    id: number;
+    @IsNumber()
+    userId: number;
 }
 
 @ObjectType()
-export class GetProductEntriesOutput extends CoreOutput {
+export class ProductEntriesOutput extends PaginationOutput {
     @Field(() => [ProductEntry])
     productEntries?: ProductEntry[];
+}
+
+
+@ObjectType()
+export class GetProductEntriesOutput extends CoreOutput {
+    @Field(() => ProductEntriesOutput, { nullable: true })
+    data?: ProductEntriesOutput;
 }
