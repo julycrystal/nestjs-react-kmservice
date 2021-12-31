44c23ee874ea6f849e94cab289391a53b32f3e5e
julycrystal <erikxins@gmail.com>
2021-12-31 20:54:35 +0630

    removed unused file

--- backend/src/product/dto/all-product.dto copy.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { CoreOutput } from "src/common/dtos/core.output";
-import { Product } from "../entities/product.entity";
-
-@ObjectType()
-export class AllProductOutput extends CoreOutput {
-    @Field(() => [Product])
-    products?: Product[];
-}
