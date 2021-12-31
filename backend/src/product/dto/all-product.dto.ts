f6c5192bc9129195f6183ed74512a793d0dd052a
julycrystal <erikxins@gmail.com>
2021-12-31 20:54:25 +0630

    pagination feature added

--- backend/src/product/dto/all-product.dto.ts
+++ backend/src/product/dto/all-product.dto.ts
@@ -1,9 +1,16 @@
 import { Field, ObjectType } from "@nestjs/graphql";
 import { CoreOutput } from "src/common/dtos/core.output";
+import { PaginationOutput } from "src/common/dtos/pagination.output";
 import { Product } from "../entities/product.entity";
 
 @ObjectType()
-export class AllProductOutput extends CoreOutput {
+export class ProductsOutput extends PaginationOutput {
     @Field(() => [Product])
     products?: Product[];
+}
+
+@ObjectType()
+export class GetProductsOutput extends CoreOutput {
+    @Field(() => ProductsOutput, { nullable: true })
+    data?: ProductsOutput;
 }
