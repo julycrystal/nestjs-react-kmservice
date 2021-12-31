883bfd58b122657a6ae46e148d2a89f740a83a67
julycrystal <erikxins@gmail.com>
2021-12-31 20:55:20 +0630

    remvoe ProductImageItem feature

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -3,12 +3,13 @@ import { ProductService } from './product.service';
 import { ProductResolver } from './product.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
-import { Product, ProductEntry, ProductImageItem } from './entities/product.entity';
+import { Product, ProductEntry } from './entities/product.entity';
 import { ProductController } from './product.controller';
 import { ProductEntryResolver } from './productEntry.resolver';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { ProductEntryService } from './productEntry.service';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ProductEntryService } from './productEntry.service';
       Category,
       Product,
       ProductImageItem,
+      User,
       ProductEntry,
     ]),
     AuthModule,
