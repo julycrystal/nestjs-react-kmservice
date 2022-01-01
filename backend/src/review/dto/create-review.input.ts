1757474d6b290d9e289b43dc5d36df462868d20f
julycrystal <erikxins@gmail.com>
2022-01-01 23:51:58 +0630

    createReview service added

--- backend/src/review/dto/create-review.input.ts
+++ backend/src/review/dto/create-review.input.ts
@@ -1,7 +1,14 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, PickType, ObjectType, Field } from '@nestjs/graphql';
+import { IsInt } from 'class-validator';
+import { CoreOutput } from 'src/common/dtos/core.output';
+import { Review } from '../entities/review.entity';
 
 @InputType()
-export class CreateReviewInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+export class CreateReviewInput extends PickType(Review, ['rating', 'comment']) {
+  @IsInt()
+  @Field(() => Number)
+  productId: number;
 }
+
+@ObjectType()
+export class CreateReviewOutput extends CoreOutput { }
--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,26 +1,75 @@
-import { Injectable } from '@nestjs/common';
-import { CreateReviewInput } from './dto/create-review.input';
-import { UpdateReviewInput } from './dto/update-review.input';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Product } from 'src/product/entities/product.entity'
+import { User } from 'src/user/entities/user.entity'
+import { Repository } from 'typeorm'
+import {
+  CreateReviewInput,
+  CreateReviewOutput,
+} from './dto/create-review.input'
+import { UpdateReviewInput } from './dto/update-review.input'
+import { Review } from './entities/review.entity'
 
 @Injectable()
 export class ReviewService {
-  create(createReviewInput: CreateReviewInput) {
-    return 'This action adds a new review';
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) { }
+
+  async createReview (
+    { rating, comment, productId }: CreateReviewInput,
+    user: User
+  ): Promise<CreateReviewOutput> {
+    try {
+      const product = await this.productRepository.findOne({ id: productId })
+      if (!product) {
+        throw new HttpException('Product not found.', HttpStatus.NOT_FOUND)
+      }
+      const review = await this.reviewRepository.create({
+        rating,
+        comment,
+        user,
+        product,
+      })
+      await this.reviewRepository.save(review)
+      return {
+        ok: true,
+      }
+    } catch (error) {
+      if (error.name && error.name === 'HttpException') {
+        throw error
+      }
+      if (error.code && error.code === '23505') {
+        return {
+          ok: false,
+          error: `This product is already revied by this user.`
+        }
+      }
+      return {
+        ok: false,
+        error: 'Cannot create review.',
+      }
+    }
   }
 
-  findAll() {
-    return `This action returns all review`;
+  findReviewsByProductId () {
+    return `This action returns all review`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  findOne (id: number) {
+    return `This action returns a #${id} review`
   }
 
-  update(id: number, updateReviewInput: UpdateReviewInput) {
-    return `This action updates a #${id} review`;
+  update (id: number, updateReviewInput: UpdateReviewInput) {
+    return `This action updates a #${id} review`
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+  remove (id: number) {
+    return `This action removes a #${id} review`
   }
 }
