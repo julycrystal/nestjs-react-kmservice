1547eeb90314b7c6959589d1b8846d696f5fff1a
julycrystal <erikxins@gmail.com>
2022-01-02 13:09:06 +0630

    page not found design fixed

--- frontend/src/components/PageNotFound.tsx
+++ frontend/src/components/PageNotFound.tsx
@@ -4,20 +4,19 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
     return (<div className="items-center justify-center">
-        <div className="bg-white shadow-lg mt-16 w-1/2 mx-auto">
-            <div className="flex p-10 items-center justify-center space-x-12">
+        <div className="bg-white shadow-lg mt-16 lg:w-2/5 mx-6 lg:mx-auto">
+            <div className="flex lg:flex-row flex-col lg:p-10 p-6 items-center justify-center lg:space-x-6">
                 <div>
-                    <FontAwesomeIcon icon={faFrown} size="6x" />
+                    <FontAwesomeIcon icon={faFrown} className="lg:text-9xl text-6xl" />
                 </div>
-                <div className="space-y-3">
-                    <p className="text-6xl font-bold">404</p>
-                    <p className="uppercase text-lg">OOPS!page not be found</p>
-                    <p className="capitalize font-semibold text-justify text-gray-500 mb-6">
-                        sorry but the page you are looking for doesn't exists,<br />
-                        have been moved, name changed or is temporarily<br />
-                        unavailable.
+                <div className="space-y-3 text-center lg:text-left mx-auto">
+                    <p className="lg:text-6xl text-3xl font-bold">404</p>
+                    <p className="uppercase lg:text-lg">OOPS!page not be found</p>
+                    <p className="capitalize font-semibold text-justify text-gray-500 mb-6 text-sm">
+                        sorry but the page you are looking for doesn't exists,
+                        have been moved, name changed or is temporarily unavailable.
                     </p>
-                    <div className="w-1/2 bg-black text-white px-4 py-2 uppercase mt-4">
+                    <div className="lg:w-2/3 w-full text-center  bg-black text-white px-4 py-2 uppercase mt-4">
                         <Link replace to="/">Back to homepage</Link>
                     </div>
                 </div>
