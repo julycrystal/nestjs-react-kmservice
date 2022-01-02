de24bc0f3985c9bca56e7dcbccf7fd2fe067623c
julycrystal <erikxins@gmail.com>
2022-01-02 20:19:32 +0630

    add to cart functionality and show button based on state feature added

--- frontend/src/features/products/components/ProductItem.tsx
+++ frontend/src/features/products/components/ProductItem.tsx
@@ -1,26 +1,72 @@
-import { faCartPlus, faEye, faStar } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useNavigate } from "react-router"
-import { GetProducts_getProducts_data_products } from "../../../__generated__/GetProducts"
+import { faCartPlus, faEye, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { RootState } from "../../../app/store";
+import { GetProducts_getProducts_data_products } from "../../../__generated__/GetProducts";
+import { removeCartItem, addToCart } from "../cart.slice";
 
 interface IProductItem {
-    product: GetProducts_getProducts_data_products,
+    product: GetProducts_getProducts_data_products;
 }
 const ProductItem = ({ product }: IProductItem) => {
+    const dispatch = useDispatch();
+    const cartItems = useSelector((state: RootState) => state.cart.items);
+
     const navigate = useNavigate();
     const detailClickHandler = (id: number) => {
         navigate(`${id}`);
+    };
+
+    const isInCart = () => {
+        return cartItems.some((y) => y.product.id === product.id);
+    };
+    const handleRemoveCart = () => {
+        dispatch(removeCartItem(product));
+    };
+
+    const handleAddToCart = () => {
+        dispatch(addToCart({ product, quantity: 1 }));
+    };
+
+    const getActionButton = () => {
+        if (product.quantity < 1) {
+            return <button className="w-full bg-gray-500 text-white pt-2 pb-1">
+                Out of Stock
+            </button>
+        }
+        if (product.quantity > 0) {
+            if (isInCart()) {
+                return <button onClick={handleRemoveCart} className="w-full bg-black text-white pt-2 pb-1">
+                    Remove from cart
+                </button>;
+            } else {
+                return <button onClick={handleAddToCart} className="w-full bg-black text-white pt-2 pb-1">
+                    Add to cart
+                </button>;
+            }
+        }
     }
+
     return (
         <div className="lg:w-1/4 md:w-1/2 w-full px-2 mb-6">
             <div className="bg-white shadow-xl pb-5">
-                <div>
-                    {product.coverImage ?
-                        <img src={product.coverImage} alt={product.title} className="h-60 w-full" /> :
-                        <img src="place-holder.png" alt={product.title} className="h-60 w-full" />
-                    }
+                <div className="cursor-pointer" onClick={() => detailClickHandler(product.id)}>
+                    {product.coverImage ? (
+                        <img
+                            src={product.coverImage}
+                            alt={product.title}
+                            className="h-60 w-full"
+                        />
+                    ) : (
+                        <img
+                            src="place-holder.png"
+                            alt={product.title}
+                            className="h-60 w-full"
+                        />
+                    )}
                 </div>
-                <div className="px-4">
+                <div className="px-4 cursor-pointer" onClick={() => detailClickHandler(product.id)}>
                     <div className="flex my-2 space-x-3">
                         <div className="flex items-center space-x-2">
                             <FontAwesomeIcon icon={faStar} className="text-xs" />
@@ -30,25 +76,15 @@ const ProductItem = ({ product }: IProductItem) => {
                             {product.category.name}
                         </p>
                     </div>
-                    <p className="">
-                        {product.title}
-                    </p>
-                    <p className="font-bold text-lg">
-                        $ {product.price}
-                    </p>
-                    {/* <hr className="border-black my-3" /> */}
-                    <div className="flex space-x-2 mt-3">
-                        <button className="w-full bg-black text-white py-1">
-                            <FontAwesomeIcon icon={faCartPlus} />
-                        </button>
-                        <button onClick={() => detailClickHandler(product.id)} className="w-full border-black border-2">
-                            <FontAwesomeIcon icon={faEye} className="" />
-                        </button>
-                    </div>
+                    <p className="">{product.title}</p>
+                    <p className="font-bold text-lg">$ {product.price}</p>
+                </div>
+                <div className="flex space-x-2 mt-3 px-4">
+                    {getActionButton()}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProductItem
+export default ProductItem;
