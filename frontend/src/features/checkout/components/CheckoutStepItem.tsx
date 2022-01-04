416847f3a5d535cce4a5832b257757b72abf17de
julycrystal <erikxins@gmail.com>
2022-01-04 17:56:32 +0630

    remove cursor pointer lclass

--- frontend/src/features/checkout/components/CheckoutStepItem.tsx
+++ frontend/src/features/checkout/components/CheckoutStepItem.tsx
@@ -6,7 +6,7 @@ interface ICheckoutStepItemProps {
 
 const CheckoutStepItem = ({ text, isActive = false }: ICheckoutStepItemProps) => {
     return (
-        <div className={`${isActive ? "bg-black" : "bg-gray-500"} cursor-pointer text-white rounded-full px-3 py-1 w-fit`}>
+        <div className={`${isActive ? "bg-black" : "bg-gray-500"} text-white rounded-full px-3 py-1 w-fit`}>
             {text}
         </div>
     )
