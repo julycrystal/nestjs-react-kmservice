14e76cedb463c23c11fcef8b1067caf768708625
julycrystal <erikxins@gmail.com>
2022-01-06 12:03:00 +0630

    redirect if not admin

--- frontend/src/features/profile/container/admiin/index.tsx
+++ frontend/src/features/profile/container/admiin/index.tsx
@@ -2,8 +2,15 @@ import { Navigate, Route, Routes } from "react-router"
 import Order from "./Order";
 import Users from "./users";
 import Products from "./Products";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../app/store";
+import { UserRole } from "../../../../__generated__/globalTypes";
 
 const Admin = () => {
+    const user = useSelector((state: RootState) => state.auth.user);
+    if (user && user.role === UserRole.User) {
+        return <Navigate to="/404" replace={true} />
+    }
     return (
         <Routes>
             <Route path="orders/*" element={<Order />} />
