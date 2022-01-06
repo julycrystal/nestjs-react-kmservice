b4d9db7f3bdecf85887fbe6e1186aa4137b84ad6
julycrystal <erikxins@gmail.com>
2022-01-06 11:06:00 +0630

    order user detail added

---
+++ frontend/src/features/profile/components/admin/order/UserDetails.tsx
@@ -0,0 +1,29 @@
+import { GetOrder_getOrder_order_customer } from "../../../../../__generated__/GetOrder";
+
+interface IUserDetailsProps {
+    user: GetOrder_getOrder_order_customer;
+}
+const UserDetails = ({ user }: IUserDetailsProps) => {
+    return (
+        <div className="ml-8">
+            <h1 className="mb-3 text-lg text-gray-700 font-bold">User Details</h1>
+            <hr className="border-gray-500" />
+            <div className="space-y-4 text-sm mt-8">
+                <div className="flex justify-between">
+                    <p>Name</p>
+                    <p>{user.name}</p>
+                </div>
+                <div className="flex justify-between">
+                    <p>Email</p>
+                    <p>{user.email}</p>
+                </div>
+                <div className="flex justify-between">
+                    <p>Username</p>
+                    <p>{user.username}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserDetails;
